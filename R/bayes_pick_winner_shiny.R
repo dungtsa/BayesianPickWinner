f73e7ffe0a4119aa496e0ebb1e3ca84f5304a632
Thompson, Zachary J <zachary.thompson@moffitt.org>
2021-03-18 15:29:42 -0400

    Updating with multiple arm design.

--- R/bayes_pick_winner_shiny.R
+++ R/bayes_pick_winner_shiny.R
@@ -1,6 +1,7 @@
 #' @name bayes.pick.winner
 #' @title Bayesian pick the winner design
-#' @description This function will run Shiny application for the Bayesian pick the winner design in randomized phase II trial
+#' @description This function will run Shiny application for the Bayesian pick the winner design in randomized phase II trials.
+#'              two-arm stage one and two and a two stage mulitple arm design.
 #' @export
 
 bayes.pick.winner <- function() {
@@ -9,7 +10,8 @@ bayes.pick.winner <- function() {
     stop("Could not find example directory. Try re-installing `BayesianPickWinner`.", call. = FALSE)
   }
 
-  shiny::runApp(paste(appDir,'/bayes_pick_winner_shiny_applictaion.R',sep=''), launch.browser =T,host = getOption( "127.0.0.1"))
+  shiny::runApp(paste(appDir,'/bayes_pick_winner_shiny_application.R',sep = ''), launch.browser = T,
+                host = getOption( "127.0.0.1"))
 }
 
 
